f37b1d4ad568bee9be412e833d9e30cdad9e131b
Krzysztof <krzysztofsmusz@gmail.com>
2019-08-16 15:19:17 +0200

    Added a module for creating a Bag of Words, tokenizing the article.

--- Wrapper/main.py
+++ Wrapper/main.py
@@ -1,6 +1,8 @@
 import urllib.request
 import URLlister
 import ContentRetriever
+import json
+import BagOfWordsCreater
 
 
 def create_socket(url):
@@ -8,24 +10,41 @@ def create_socket(url):
                                                          headers={'User-Agent': 'Mozilla/5.0'}))
 
 
+def write_string_to_json(articleTXT, filename):
+    with open(filename, 'w') as outfile:
+        json.dump(articleTXT, outfile)
+
+
+def open_json_to_string(articleTXT):
+    with open(articleTXT) as infile:
+        return json.load(infile)
+
+
 if __name__ == '__main__':
+    tryb = 2
+    if tryb == 1:
+        sock = create_socket('https://geek.justjoin.it')
+        MainPage = URLlister.WrapperURLHelper(sock)
+        sock.close()
 
-    sock = create_socket('https://geek.justjoin.it')
-    MainPage = URLlister.WrapperURLHelper(sock)
-    sock.close()
+        for i, url in enumerate(MainPage.articlesUrls):
+            if i == 2:
+                sock = create_socket(url)
 
-    for i, url in enumerate(MainPage.articlesUrls):
-        if i == 2:
-            sock = create_socket(url)
+                print(url)
+                article = ContentRetriever.WrapperArticleParser(sock.read())
 
-            print(url)
-            article = ContentRetriever.WrapperArticleParser(sock.read())
+                sock.close
 
-            sock.close
+                write_string_to_json(article.articleText, 'article.txt')
 
-            for cat in article.articleCategories:
-                print(cat)
-            for text in article.articleText:
-                print(text[0])
-            break
-
+                for cat in article.articleCategories:
+                    print(cat)
+                for text in article.articleText:
+                    print(text[0])
+                break
+    else:
+        artText = open_json_to_string("article.txt")
+        bag = BagOfWordsCreater.BagOfWords([text[0] for text in artText])
+        print([text[0] for text in artText])
+        print(bag.articleExtracted)
